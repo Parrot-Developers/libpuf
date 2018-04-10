526acb55de1a668cfc0df14519abe28ea8ac9380
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2018-04-10 10:54:29 +0200

    [DEV] Add test code.
    - Only tar archive tested for now.

--- atom.mk
+++ atom.mk
@@ -17,3 +17,27 @@ LOCAL_CONDITIONAL_LIBRARIES := \
 	OPTIONAL:libulog
 
 include $(BUILD_LIBRARY)
+
+ifdef TARGET_TEST
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := tst-libpuf
+LOCAL_LIBRARIES := libcunit libpuf
+
+LOCAL_SRC_FILES := \
+	tests/puf_test.c \
+	tests/puf_test_tar.c
+
+LOCAL_COPY_FILES := \
+	tests/tar/archive1.tar:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/archive1-truncated.tar:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/archive2.tar.gz:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/archive2-truncated.tar.gz:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/archive3.tar.bz2:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/empty.tar:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+	tests/tar/empty.tar.gz:$(TARGET_DEFAULT_BIN_DESTDIR)/ \
+
+include $(BUILD_EXECUTABLE)
+
+endif
